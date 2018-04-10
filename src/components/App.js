56690a679036be6c5624d3704b519df41b67bb93
alfio parisi <alfioparisi.93@gmail.com>
2018-04-10 16:21:26 +0100

    App first tests and rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from '../logo.svg';
 import '../App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import Paper from 'material-ui/Paper';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <div>
+          <AppBar />
+          <Paper />
+          <FloatingActionButton style={{
+            position: 'absolute',
+            bottom: 15,
+            right: 15
+          }}>
+            <ContentAdd />
+          </FloatingActionButton>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,9 +1,57 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from '../App';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import Paper from 'material-ui/Paper';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  let props;
+  let mountedApp;
+
+  function app() {
+    if (!mountedApp) mountedApp = shallow(<App {...props} />);
+    return mountedApp;
+  }
+
+  beforeEach(() => {
+    props = {};
+    mountedApp = undefined;
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should always render a `MuiThemeProvider`', () => {
+    const muiThemeProvider = app().find(MuiThemeProvider);
+    expect(muiThemeProvider.length).toBe(1);
+  });
+  describe('the rendered `MuiThemeProvider`', () => {
+    it('should contain everything else', () => {
+      const muiThemeProvider = app().find(MuiThemeProvider);
+      expect(muiThemeProvider.children()).toEqual(app().children());
+    });
+  });
+
+  it('should always render an `AppBar`', () => {
+    expect(app().find(AppBar).length).toBe(1);
+  });
+
+  it('should always render a `Paper`', () => {
+    expect(app().find(Paper).length).toBe(1);
+  });
+
+  it('should always render a `FloatingActionButton`', () => {
+    expect(app().find(FloatingActionButton).length).toBe(1);
+  });
+
 });
