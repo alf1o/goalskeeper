988978ef06d2038064e2e197c4bdf2b1e052c8a0
alfio parisi <alfioparisi.93@gmail.com>
2018-04-11 14:30:30 +0100

    Remove GoalSmall Component

--- src/components/Goal.js
+++ src/components/Goal.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import GoalSmall from './GoalSmall';
+import { ListItem } from 'material-ui/List';
+import ArrowDropDown from 'material-ui/svg-icons/navigation/arrow-drop-down';
+import LinearProgress from 'material-ui/LinearProgress';
+import FlatButton from 'material-ui/FlatButton';
+import Add from 'material-ui/svg-icons/content/add';
 
 class Goal extends Component {
   static propTypes = {
@@ -8,14 +12,36 @@ class Goal extends Component {
   };
 
   state = {
-    expanded: false
+    expanded: false,
+    progress: 0
   };
+
+  componentDidMount() {
+    const { steps } = this.props.goal;
+    if (steps.length) {
+      const progress = steps.filter(step => step.completed).length / steps.length * 100;
+      this.setState({ progress });
+    }
+  }
 
   render() {
     const { goal } = this.props;
-    const { expanded } = this.state;
+    const { expanded, progress } = this.state;
     return (
-      !expanded && <GoalSmall goal={goal} />
+      <div>
+        <ListItem
+          leftIcon={<ArrowDropDown />}
+          onClick={() => alert('Expand me')}
+          primaryText={goal.label}
+        />
+        <LinearProgress mode='determinate' value={progress} />
+        <FlatButton
+          label='add a step'
+          icon={<Add />}
+          onClick={() => alert('Add a step')}
+          fullWidth={true}
+        />
+      </div>
     );
   }
 }
