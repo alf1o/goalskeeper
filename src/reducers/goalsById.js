3b8fde511e7e66bb4168add8d72d2f70f621197c
alfio parisi <alfioparisi.93@gmail.com>
2018-04-21 16:00:25 +0100

    Goal reducer handles CREATE_GOAL

--- src/reducers/goalsById.js
+++ src/reducers/goalsById.js
@@ -6,20 +6,29 @@ import {
 } from '../actions/types';
 import { formattedDate } from '../utils';
 
+function goal(state = {}, action) {
+  switch(action.type) {
+    case CREATE_GOAL:
+      return {
+        id: action.id,
+        name: action.name,
+        dueDate: action.dueDate,
+        steps: [],
+        completed: false,
+        dateCreated: formattedDate(),
+        description: action.description
+      };
+    default:
+      return state;
+  }
+}
+
 function goalsById(state = {}, action) {
   switch(action.type) {
     case CREATE_GOAL:
       return {
         ...state,
-        [action.id]: {
-          id: action.id,
-          name: action.name,
-          dueDate: action.dueDate,
-          steps: [],
-          completed: false,
-          dateCreated: formattedDate(),
-          description: action.description
-        }
+        [action.id]: goal(undefined, action)
       };
     default:
       return state;
