2f69e4d4e6deb707d270a171e5a653eee48cf409
alfio parisi <alfioparisi.93@gmail.com>
2018-04-23 14:51:46 +0100

    Use Provider

--- src/index.js
+++ src/index.js
@@ -2,65 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
-const mockState = {
-  goalsById: {
-    'goal_0': {
-      label: 'Finish React static app',
-      id: 'goal_0',
-      dueDate: '15/04/2018',
-      steps: [
-        {
-          id: 'step_0_0',
-          content: 'Read React docs',
-          completed: true,
-          dateCompleted: '09/04/2018'
-        },
-        {
-          id: 'step_0_1',
-          content: 'Learn about Component testing',
-          completed: false,
-          dateCompleted: null
-        }
-      ],
-      completed: false,
-      dateCreated: '09/04/2018',
-      description: 'Would like to build my own goal keeper'
-    },
-    'goal_1': {
-      label: 'Add Redux to the app',
-      id: 'goal_1',
-      dueDate: '22/04/2018',
-      steps: [
-        {
-          id: 'step_1_0',
-          content: 'Read Redux docs',
-          completed: false,
-          dateCompleted: null
-        },
-        {
-          id: 'step_1_1',
-          content: 'Learn about reducers testing',
-          completed: false,
-          dateCompleted: null
-        }
-      ],
-      completed: false,
-      dateCreated: '09/04/2018',
-      description: 'Would like to build my own goal keeper'
-    }
-  },
-  goals: ['goal_0', 'goal_1'],
-  sortedBy: 'LAST_CREATED',
-  goalsCompleted: [],
-  quoteOfTheDay: 'The limit is not in the art, but in the man that practices it.'
-};
-
 ReactDOM.render(
-  <BrowserRouter>
-    <App mockState={mockState} />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { createLogger } from 'redux-logger';
+import goalsById from './reducers/goalsById';
+import stepsById from './reducers/stepsById';
 
 // If Redux DevTools extension is available, use it.
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -9,7 +11,8 @@ const logger = createLogger({
 });
 
 const reducer = combineReducers({
-
+  goalsById,
+  stepsById
 });
 
 const store = createStore(
@@ -27,20 +30,7 @@ export default store;
       label: 'Finish React static app',
       id: 'goal_0',
       dueDate: '15/04/2018',
-      steps: [
-        {
-          id: 'step_0_0',
-          content: 'Read React docs',
-          completed: true,
-          dateCompleted: '09/04/2018'
-        },
-        {
-          id: 'step_0_1',
-          content: 'Learn about Component testing',
-          completed: false,
-          dateCompleted: null
-        }
-      ],
+      steps: ['step_0_1', 'step_0_2'],
       completed: false,
       dateCreated: '09/04/2018',
       description: 'Would like to build my own goal keeper'
@@ -49,21 +39,8 @@ export default store;
       label: 'Add Redux to the app',
       id: 'goal_1',
       dueDate: '22/04/2018',
-      steps: [
-        {
-          id: 'step_1_0',
-          content: 'Read Redux docs',
-          completed: false,
-          dateCompleted: null
-        },
-        {
-          id: 'step_1_1',
-          content: 'Learn about reducers testing',
-          completed: false,
-          dateCompleted: null
-        }
-      ],
-      completed: false,
+      steps: [],
+      completed: true,
       dateCreated: '09/04/2018',
       description: 'Would like to build my own goal keeper'
     }
@@ -71,6 +48,22 @@ export default store;
   goals: ['goal_0', 'goal_1'],
   sortedBy: 'LAST_CREATED',
   goalsCompleted: [],
-  quoteOfTheDay: 'The limit is not in the art, but in the man that practices it.'
+  quoteOfTheDay: 'The limit is not in the art, but in the man that practices it.',
+  stepsByid: {
+    step_0_0: {
+      id: 'step_0_0',
+      content: 'Read React docs',
+      completed: true,
+      dateCompleted: '09/04/2018',
+      goalId: 'id_0'
+    },
+    step_0_1: {
+      id: 'step_0_1',
+      content: 'Learn about Component testing',
+      completed: false,
+      dateCompleted: null,
+      goalId: 'id_0'
+    }
+  }
 }
 */
