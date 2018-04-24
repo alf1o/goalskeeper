2e0d3be564e4f2e3c14b5dd470938e187a962ac1
alfio parisi <alfioparisi.93@gmail.com>
2018-04-24 15:33:43 +0100

    AddStepModal is a controlled Component

--- src/components/AddStepModal.js
+++ src/components/AddStepModal.js
@@ -1,21 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'material-ui/Modal';
 import Paper from 'material-ui/Paper';
 import Typography from 'material-ui/Typography';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+import { Redirect } from 'react-router-dom';
 
-function AddStepModal({ open, onClose }) {
-  return (
-    <Modal>
+class AddStepModal extends Component {
+  static propTypes = {
+    open: PropTypes.bool.isRequired,
+    onClose: PropTypes.func.isRequired
+  };
 
-    </Modal>
-  );
+  constructor() {
+    super();
+    this.state = {
+      content: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(evt) {
+    this.setState({ content: evt.target.value });
+  }
+
+  render() {
+    const { content } = this.state;
+    const { open, onClose } = this.props;
+    return (
+      <Modal
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        open={open}
+        onClose={onClose}
+      >
+        <Paper>
+          <Typography variant="title">Add a new step</Typography>
+          <TextField
+            autoFocus={true}
+            error={false}
+            id="add-step"
+            label="Content: "
+            margin="normal"
+            name="add-step"
+            onChange={this.handleChange}
+            placeholder="A journey of a thousand miles starts with the first step"
+            required={true}
+            value={content}
+          />
+          <Button
+            variant="raised"
+            color="primary"
+            style={{margin: 'auto'}}
+            onClick={() => alert('Add')}
+            disabled={!content}
+          >
+            Add
+          </Button>
+        </Paper>
+      </Modal>
+    );
+  }
 }
-
-AddStepModal.propTypes = {
-  open: PropTypes.bool.isRequired,
-  onClose: PropTypes.func.isRequired
-};
 
 export { AddStepModal as UnwrappedAddStepModal };
 export default AddStepModal;
--- src/components/__tests__/AddStepModal.test.js
+++ src/components/__tests__/AddStepModal.test.js
@@ -8,6 +8,10 @@ import { UnwrappedAddStepModal } from '../AddStepModal';
 import Modal from 'material-ui/Modal';
 import Paper from 'material-ui/Paper';
 import Typography from 'material-ui/Typography';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+import Card from 'material-ui/Card';
+import { Redirect } from 'react-router-dom';
 
 describe('`AddStepModal`', () => {
   let props;
@@ -34,6 +38,94 @@ describe('`AddStepModal`', () => {
       const modal = addStepModal().find(Modal);
       expect(addStepModal().children()).toEqual(modal.children());
     });
+
+    it('should receive an `open` prop', () => {
+      const modal = addStepModal().find(Modal);
+      expect(modal.props().open).toBeDefined();
+    });
+
+    it('should receive an `onClose` prop', () => {
+      const modal = addStepModal().find(Modal);
+      expect(modal.props().onClose).toBeDefined();
+    });
   });
+
+  it('should contain a `Paper`', () => {
+    expect(addStepModal().find(Paper).length).toBe(1);
+  });
+
+  it('should contain a `Typography`', () => {
+    expect(addStepModal().find(Typography).length).toBe(1);
+  });
+
+  it('should contain a `TextField`', () => {
+    expect(addStepModal().find(TextField).length).toBe(1);
+  });
+  describe('the rendered `TextField`', () => {
+    it('should receive an `onChange` prop', () => {
+      const textField = addStepModal().find(TextField);
+      expect(textField.props().onChange).toBeDefined();
+    });
+
+    it('should receive a `value` prop', () => {
+      const textField = addStepModal().find(TextField);
+      expect(textField.props().value).toBeDefined();
+    });
+  });
+
+  it('should contain a `Button`', () => {
+    expect(addStepModal().find(Button).length).toBe(1);
+  });
+  describe('the rendered `Button`', () => {
+    it('should receive an `onClick` prop', () => {
+      const btn = addStepModal().find(Button);
+      expect(btn.props().onClick).toBeDefined();
+    });
+  });
+
+  it('should have a `state.content` property', () => {
+    expect(addStepModal().state().content).toBeDefined();
+  });
+  describe('when `state.content` is empty', () => {
+    it('should disable the `Button`', () => {
+      const btn = addStepModal().find(Button);
+      expect(btn.props().disabled).toBe(true);
+    });
+  });
+  describe('when `state.content` is not empty', () => {
+    beforeEach(() => {
+      addStepModal().setState({ content: 'test' });
+    });
+
+    it('should enable the `Button`', () => {
+      const btn = addStepModal().find(Button);
+      expect(btn.props().disabled).toBe(false);
+    });
+  });
+
+  it('should have an `handleChange` method', () => {
+    expect(UnwrappedAddStepModal.prototype.handleChange).toBeDefined();
+  });
+  describe('the `handleChange` method', () => {
+    const handleChangeSpy = jest.spyOn(UnwrappedAddStepModal.prototype, 'handleChange');
+    afterEach(() => {
+      handleChangeSpy.mockClear();
+    });
+
+    it('should be called on `TextField` change', () => {
+      const textField = addStepModal().find(TextField);
+      textField.simulate('change', { target: {value: ''} });
+      expect(handleChangeSpy.mock.calls.length).toBe(1);
+    });
+
+    it('should change `state.content`', () => {
+      expect(addStepModal().state().content).toBe('');
+
+      const textField = addStepModal().find(TextField);
+      textField.simulate('change', { target: {value: 'test'} });
+      expect(addStepModal().state().content).toBe('test');
+    });
+  });
+  // handleClick
 
 });
