5e6744f836ea24c072a1cc26e5067d926b93dc39
alfio parisi <alfioparisi.93@gmail.com>
2018-04-25 15:57:17 +0100

    Step is stateless functional

--- src/components/Step.js
+++ src/components/Step.js
@@ -1,58 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 import Checkbox from 'material-ui/Checkbox';
-import Clear from '@material-ui/icons/Clear';
-import Done from '@material-ui/icons/Done';
 import { connect } from 'react-redux';
 import completeStep from '../actions/completeStep';
 
-class Step extends Component {
-  static propTypes = {
-    step: PropTypes.object.isRequired,
-    completeStep: PropTypes.func.isRequired
-  };
+const Step = ({ step, completeStep }) => (
+  <ListItem
+    component="button"
+    onClick={() => completeStep(step.id, !step.completed)}
+  >
+    <Checkbox
+      checked={step.completed}
+      disableRipple
+      color='primary'
+    />
+    <ListItemText inset primary={step.content} />
+  </ListItem>
+);
 
-  constructor() {
-    super();
-    this.state = {
-      completed: false
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  componentDidMount() {
-    const { completed } = this.props.step;
-    this.setState({ completed });
-  }
-
-  handleClick() {
-    const { step, completeStep } = this.props;
-    this.setState(oldState => ({ completed: !oldState.completed }));
-    completeStep(step.id, !this.state.completed);
-  }
-
-  render() {
-    const { step } = this.props;
-    const { completed } = this.state;
-    return (
-      <ListItem
-        component="button"
-        onClick={this.handleClick}
-      >
-        <Checkbox
-          checked={completed}
-          disableRipple
-          color='primary'
-        />
-        <ListItemText inset primary={step.content} />
-        <ListItemIcon>
-          {completed ? <Done /> : <Clear />}
-        </ListItemIcon>
-      </ListItem>
-    );
-  }
-}
+Step.propTypes = {
+  step: PropTypes.object.isRequired,
+  completeStep: PropTypes.func.isRequired
+};
 
 const mapDispatchToProps = {
   completeStep
--- src/components/__tests__/Step.test.js
+++ src/components/__tests__/Step.test.js
@@ -40,103 +40,27 @@ describe('Step', () => {
     beforeEach(() => {
       listItem = mountStep().find(ListItem);
     });
+
     it('should receive a `onClick` prop', () => {
       expect(listItem.props().onClick).toBeDefined();
     });
-    it('should render a `Checkbox`', () => {
-      expect(listItem.find(Checkbox).length).toBe(1);
-    });
-    describe('the rendered `Checkbox`', () => {
-      it('should receive a `checked` prop', () => {
-        const checkbox = listItem.find(Checkbox);
-        expect(checkbox.props().checked).toBeDefined();
-      });
-    });
   });
 
-  it('should have a `state.completed`', () => {
-    expect(mountStep().state().completed).toBeDefined();
+  it('should render a `Checkbox`', () => {
+    expect(mountStep().find(Checkbox).length).toBe(1);
   });
-
-  it('should call `componentDidMount` once', () => {
-    const componentDidMountSpy = jest.spyOn(UnwrappedStep.prototype, 'componentDidMount');
-    mountStep();
-    expect(componentDidMountSpy.mock.calls.length).toBe(1);
-  });
-
-  it('should update the `state` with the value of `step.completed` prop when mounted', () => {
-    mountStep();
-    expect(mountStep().state().completed).toEqual(mountStep().instance().props.step.completed);
-  });
-
-  describe('when `state.completed` is `false`', () => {
-    beforeEach(() => {
-      mountStep().setState({ completed: false });
-    });
-    it('should pass `false` as the `checked` value of the `Checkbox`', () => {
+  describe('the rendered `Checkbox`', () => {
+    it('should receive a `checked` prop with the value of `step.completed`', () => {
       const checkbox = mountStep().find(Checkbox);
-      expect(checkbox.props().checked).toBe(false);
-    });
-    it('should render a `Clear` icon inside `ListItem`', () => {
-      const clear = mountStep().find(ListItem).find(Clear);
-      expect(clear.length).toBe(1);
-    });
-    it('should not render a `Done` icon inside `ListItem`', () => {
-      const doneIcon = mountStep().find(ListItem).find(Done);
-      expect(doneIcon.length).toBe(0);
+      expect(checkbox.props().checked).toBeDefined();
+
+      expect(checkbox.props().checked).toBe(props.step.completed);
     });
   });
 
-  describe('when `state.completed` is `true`', () => {
-    beforeEach(() => {
-      mountStep().setState({ completed: true });
-    });
-    it('should pass `true` as the `checked` value of the `Checkbox`', () => {
-      const checkbox = mountStep().find(Checkbox);
-      expect(checkbox.props().checked).toBe(true);
-    });
-    it('should render a `Done` icon inside `ListItem`', () => {
-      const doneIcon = mountStep().find(ListItem).find(Done);
-      expect(doneIcon.length).toBe(1);
-    });
-    it('should not render a `Clear` icon inside `ListItem`', () => {
-      const clear = mountStep().find(ListItem).find(Clear);
-      expect(clear.length).toBe(0);
-    });
-  });
-
-  it('should have an `handleClick` method', () => {
-    expect(UnwrappedStep.prototype.handleClick).toBeDefined();
-  });
-  describe('`handleClick`', () => {
-    let handleClickSpy;
-    beforeEach(() => {
-      handleClickSpy = jest.spyOn(UnwrappedStep.prototype, 'handleClick');
-    });
-    afterEach(() => {
-      handleClickSpy.mockClear();
-    });
-
-    it('should be called on `ListItem` click', () => {
-      const listItem = mountStep().find(ListItem);
-      listItem.simulate('click');
-      expect(handleClickSpy.mock.calls.length).toBe(1);
-    });
-
-    it('should flip the value of `state.completed`', () => {
-      const listItem = mountStep().find(ListItem);
-      let completedState = mountStep().state().completed;
-      listItem.simulate('click');
-      expect(mountStep().state().completed).toBe(!completedState);
-      listItem.simulate('click');
-      expect(mountStep().state().completed).toBe(completedState);
-    });
-
-    it('should dispatch `COMPLETE_STEP`', () => {
-      const listItem = mountStep().find(ListItem);
-      listItem.simulate('click');
-      expect(mountStep().instance().props.completeStep.mock.calls.length).toBe(1);
-    });
+  it('should dispatch a `COMPLETE_STEP` action when the `ListItem` is clicked', () => {
+    mountStep().find(ListItem).simulate('click');
+    expect(props.completeStep.mock.calls.length).toBe(1);
   });
 
 });
